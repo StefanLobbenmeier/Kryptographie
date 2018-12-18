023c1aceae74f52d91c73aab2564195450a4a42e
Stefan Lobbenmeier <stefanlobbenmeier@Stefans-MacBook.fritz.box>
2018-12-18 22:19:21 +0100

    Created smallstepgiantstep project

---
+++ smallstepgiantstep/src/main/java/com/fhswf/kryptographie/App.java
@@ -0,0 +1,13 @@
+package com.fhswf.kryptographie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
